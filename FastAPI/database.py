90be086038edd5a23b4d6c8b40b1e3d46167ada7
ShodiqImawan <shodiqsantuy@gmail.com>
2025-06-23 09:37:31 +0700

    Update database.py

--- FastAPI/database.py
+++ FastAPI/database.py
@@ -21,9 +21,13 @@ DB_NAME = os.getenv('DB_NAME')
 # Membuat database url untuk membuka koneksi ke database menggunakan sqlalchemy
 DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 
-# 
+# create_engine() digunakan untuk membuat objek engine yang akan dipakai SQLAlchemy
+# Engine ini akan terhubung ke database sesuai alamat yang ditentukan di DATABASE_URL
 engine = create_engine(DATABASE_URL)
 
+# sessionmaker() mengembalikan kelas pembuat sesi database (bukan objeknya langsung)
+# Setiap kali dipanggil (misalnya dengan SessionLocal()), akan dibuat satu sesi (koneksi logis)
+# ke database sesuai konfigurasi engine dan opsi autocommit/autoflush
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
